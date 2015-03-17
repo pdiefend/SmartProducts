1953c5542083a6bd89a27af73e6544b7fbb016d0
Phil Diefenderfer <pdiefend@ptd.net>
2015-03-16 21:25:27 -0400

    Basic interruptCounter example works

--- NativeWorkspace/interruptCounter/flasher.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-python /opt/Espressif/esptool-py/esptool.py --port /dev/ttyUSB0 write_flash 0x00000 firmware/0x00000.bin 0x40000 firmware/0x40000.bin
-
--- NativeWorkspace/interruptCounter/user/user_main.c
+++ NativeWorkspace/interruptCounter/user/user_main.c
@@ -1,8 +1,14 @@
+#include "c_types.h"
+//#include "ip_addr.h"
 #include "ets_sys.h"
+//#include "espconn.h"
 #include "osapi.h"
+#include "mem.h"
 #include "gpio.h"
 #include "os_type.h"
 #include "user_config.h"
+#include "user_interface.h"
+#include "driver/uart.h"
 
 #define user_procTaskPrio        0
 #define user_procTaskQueueLen    1
@@ -11,6 +17,7 @@ static void user_procTask(os_event_t *events);
 
 static volatile os_timer_t some_timer;
 
+static uint16_t counter = 0;
 
 void some_timerfunc(void *arg)
 {
@@ -25,27 +32,48 @@ void some_timerfunc(void *arg)
         //Set GPIO2 to HIGH
         gpio_output_set(BIT2, 0, BIT2, 0);
     }
+    ETS_GPIO_INTR_DISABLE();                        // Disable gpio interrupts
+    uint16_t drop_copy = counter;
+    ETS_GPIO_INTR_ENABLE();                         // Enable gpio interrupts
+
+    os_printf("Button Pressed %d Times", drop_copy);
 }
 
 //Do nothing function
-static void ICACHE_FLASH_ATTR
-user_procTask(os_event_t *events)
-{
+static void ICACHE_FLASH_ATTR user_procTask(os_event_t *events) {
     os_delay_us(10);
 }
 
+void buttonISR(int8_t key) {
+    counter++;
+    uint32 gpio_status = GPIO_REG_READ(GPIO_STATUS_ADDRESS);
+    GPIO_REG_WRITE(GPIO_STATUS_W1TC_ADDRESS, gpio_status & BIT(0));
+}
+
+
 //Init function 
-void ICACHE_FLASH_ATTR
-user_init()
-{
-    // Initialize the GPIO subsystem.
+void ICACHE_FLASH_ATTR user_init() {
+    wifi_station_disconnect();
+    uart_init(BIT_RATE_57600, BIT_RATE_57600);    // Init UART @ 57600 bps    
+    
+    ETS_GPIO_INTR_DISABLE();
     gpio_init();
+
 
     //Set GPIO2 to output mode
     PIN_FUNC_SELECT(PERIPHS_IO_MUX_GPIO2_U, FUNC_GPIO2);
 
     //Set GPIO2 low
     gpio_output_set(0, BIT2, BIT2, 0);
+
+    ETS_GPIO_INTR_ATTACH(buttonISR, 0);          // GPIO12 interrupt handler
+    PIN_FUNC_SELECT(PERIPHS_IO_MUX_MTDI_U, FUNC_GPIO0);    // Set GPIO12 function
+    gpio_output_set(0, 0, 0, GPIO_ID_PIN(0));              // Set GPIO12 as input
+    GPIO_REG_WRITE(GPIO_STATUS_W1TC_ADDRESS, BIT(0));      // Clear GPIO12 status
+    gpio_pin_intr_state_set(GPIO_ID_PIN(0), 1);            // Interrupt on posedge
+
+    PIN_PULLUP_EN(PERIPHS_IO_MUX_MTCK_U);                   // Enable pullup
+    ETS_GPIO_INTR_ENABLE();                                 // Enable gpio interrupts
 
     //Disarm timer
     os_timer_disarm(&some_timer);
@@ -57,8 +85,9 @@ user_init()
     //&some_timer is the pointer
     //1000 is the fire time in ms
     //0 for once and 1 for repeating
-    os_timer_arm(&some_timer, 500, 1);
+    os_timer_arm(&some_timer, 1000, 1);
     
     //Start os task
     system_os_task(user_procTask, user_procTaskPrio,user_procTaskQueue, user_procTaskQueueLen);
+    os_printf("\n\rStartup done\n\r");                                  // Startup done
 }
