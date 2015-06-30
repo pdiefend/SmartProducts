33ae2711cce0068ce2e2b3974b745382049eb640
pdiefend <pdiefend@ptd.net>
2015-06-30 12:25:45 -0400

    Modified program so it only connects when it wants to make a request. Current the connection is terminated when an unknown error occurs, so I guess I;m leaving it that way. Code also parses out the current power, it isn't pretty, but I think it works.

--- NativeWorkspace/EnphaseRTD/user/user_main.c
+++ NativeWorkspace/EnphaseRTD/user/user_main.c
@@ -11,6 +11,8 @@
 #include "driver/uart.h"
 #include "ssl/cert.h"
 #include "ssl/private_key.h"
+#include "jsonparse.h"
+#include "jsontree.h"
 
 //#include "cJSON.h"
 
@@ -29,9 +31,10 @@ static volatile os_timer_t updateTimer;
 static volatile os_timer_t networkTimer;
 
 // User variables
-
+//ip_addr_t esp_server_ip;
 
 // Forward Declarations
+int atoi(char* s);
 static void user_procTask(os_event_t *events);
 void updateTimerISR(void *arg);
 void user_init();
@@ -42,19 +45,22 @@ static void tcpNetworkConnectedCb(void *arg);
 static void tcpNetworkReconCb(void *arg, sint8 err);
 static void tcpNetworkDisconCb(void *arg);
 static void init_tcp_conn(void);
+//void dns_found_CB(const char *name, ip_addr_t *ipaddr, void *arg);
 
 
 void updateTimerISR(void *arg) {
-
-
+    // Start a new connection
+    espconn_secure_connect(&global_tcp_connect);
+/*
     uint8_t data[REQUEST_LEN] = REQUEST_URL;
     
     // Send the data
     espconn_secure_sent(&global_tcp_connect, data, REQUEST_LEN);
     os_printf(data);
     os_printf("\n");
-
-    os_timer_disarm(&updateTimer);
+*/
+    // disarm the timer for now since I'm mostly doing one-shot tests
+    // os_timer_disarm(&updateTimer);
 }
 
 /* Do nothing function */
@@ -64,12 +70,30 @@ static void ICACHE_FLASH_ATTR user_procTask(os_event_t *events) {
 
 
 void ICACHE_FLASH_ATTR user_rf_pre_init() {
-    system_phy_set_rfoption(0);
+    system_phy_set_rfoption(1);
 }
 
 /* User Init, code execution starts here from OS */
 void ICACHE_FLASH_ATTR user_init() {
     uart_init(BIT_RATE_115200, BIT_RATE_115200);                    // Init UART @ 115200 bps    
+    
+    os_printf("Begin");
+    
+    /*
+    uint8_t data[RESPONSE_LEN] = RESPONSE;
+    struct jsonparse_state state;
+    jsonparse_setup(&state, data, RESPONSE_LEN);
+    
+    char buffer[50];
+    jsonparse_copy_value(&state, buffer, 50);
+    os_printf(buffer);
+    jsonparse_next(&state);
+    jsonparse_copy_value(&state, buffer, 50);
+    os_printf(buffer);
+    */
+    
+    
+    
     
     ETS_GPIO_INTR_DISABLE();                                        // Disable Interrupts
     gpio_init();                                                    // Enable GPIOS
@@ -93,6 +117,7 @@ void ICACHE_FLASH_ATTR user_init() {
     //Start os task
     system_os_task(user_procTask, user_procTaskPrio,user_procTaskQueue, user_procTaskQueueLen);
     os_printf("\n\rStartup done\n\r");
+    
 }
 
 /* network init function */
@@ -126,6 +151,16 @@ static void ICACHE_FLASH_ATTR tcpNetworkRecvCb(void *arg, char *data, unsigned s
     os_printf("Recv: %d \n\r", len);
     os_printf(data); // <=======================================================================================
     os_printf("\n");
+    
+    char* ptr = strstr(data, "current_power"); // find the 'c' in substring current_power
+    int idx = ptr-data;
+    //espconn_secure_disconnect(&global_tcp_connect);
+    // os_printf("Idx: %d\n", idx);
+    idx += 15; // offset for <current_power":> 
+    // Simple test to prove we found it. Need to actually parse integer from this
+    // os_printf("Current Power (char): %c%c%c%c \r\n", data[idx], data[idx+1], data[idx+2], data[idx+3]);
+    int power = atoi(&data[idx]);
+    os_printf("Current Power (int): %d \r\n", power);
 }
 
 /* tcp Successfully connected Call back */
@@ -137,44 +172,75 @@ static void ICACHE_FLASH_ATTR tcpNetworkConnectedCb(void *arg) {
 
     os_printf("TCP connected\n\r"); 
     tcp_conn_ok = TRUE;
+    
+    uint8_t data[REQUEST_LEN] = REQUEST_URL;
+    
+    // Send the data
+    espconn_secure_sent(&global_tcp_connect, data, REQUEST_LEN);
+    os_printf(data);
+    os_printf("\n");
 }
+
 
 /* Callback Function for when the network was reconnected, re-init stuff */
 static void ICACHE_FLASH_ATTR tcpNetworkReconCb(void *arg, sint8 err) {
-    os_printf("TCP reconnect\n\r");
+    //os_printf("TCP reconnect\n\r");
     tcp_conn_ok = FALSE;
-    /* Reset the network connections */
-    networkInit();
+    // networkInit();
 }
 
 /* Callback Function for when the network was disconnected, re-init stuff */
 static void ICACHE_FLASH_ATTR tcpNetworkDisconCb(void *arg) {
     os_printf("TCP disconnect\n\r");
     tcp_conn_ok = FALSE;
-    networkInit();
+    // networkInit();
 }
 
 /* Initialize the TCP connection */
 static void ICACHE_FLASH_ATTR init_tcp_conn(void) {
+    //espconn_gethostbyname(&global_tcp_connect, "api.enphase.com", &esp_server_ip, dns_found_CB);
+    
     global_tcp_connect.type=ESPCONN_TCP;
     global_tcp_connect.state=ESPCONN_NONE;
     global_tcp_connect.proto.tcp=&global_tcp;
     global_tcp_connect.proto.tcp->local_port=espconn_port();
 
+    // change so we go to DNS rather than hardcoded IP address
     // IP to send data to 198.101.171.58 (api.enphaseenergy.com)
     global_tcp_connect.proto.tcp->remote_ip[0]=198;
     global_tcp_connect.proto.tcp->remote_ip[1]=101;
     global_tcp_connect.proto.tcp->remote_ip[2]=171;
     global_tcp_connect.proto.tcp->remote_ip[3]=58;
-    global_tcp_connect.proto.tcp->remote_port=443; //443 or 80
+    global_tcp_connect.proto.tcp->remote_port=443; //443 for sercure or 80 for non-secure
 
     // Register Callbacks
     espconn_regist_connectcb(&global_tcp_connect, tcpNetworkConnectedCb);
     espconn_regist_disconcb(&global_tcp_connect, tcpNetworkDisconCb);
     espconn_regist_reconcb(&global_tcp_connect, tcpNetworkReconCb);
 
-    espconn_secure_set_size(ESPCONN_CLIENT, 5120); // set SSL buffer size
+    // Buffer for SSL certificate
+    espconn_secure_set_size(ESPCONN_CLIENT, 7680);
 
     // Fire it up
-    espconn_secure_connect(&global_tcp_connect);
+    // espconn_secure_connect(&global_tcp_connect);
+  
+    
+}
+/*
+void ICACHE_FLASH_ATTR dns_found_CB(const char *name, ip_addr_t *ipaddr, void *arg) {
+    struct espconn *pespconn = (struct espconn *)arg;
+    os_printf("dns_found_CB %d . %d . %d . %d/n",
+    *((uint8 *)&ipaddr->addr), *((uint8 *)&ipaddr->addr + 1),
+    *((uint8 *)&ipaddr->addr + 2), *((uint8 *)&ipaddr->addr + 3));
+}
+*/
+
+/* atoi: convert s to integer */
+/*from Kernighan and Ritchie "The C Programming Language, pg 43 */ 
+int atoi(char* s){
+    int i, n = 0;
+    
+    for (i = 0; s[i] >= '0' && s[i] <= '9'; i++)
+        n = 10 * n + (s[i] - '0');
+    return n;
 }
